c9cb90b7f390b9bd5b2898b6a317aae63af7820f
Theodor-Bjornslatt <77094997+Theodor-Bjornslatt@users.noreply.github.com>
2021-12-22 14:02:22 +0100

    Added variables

--- client/styles/globalStyle.tsx
+++ client/styles/globalStyle.tsx
@@ -1,7 +1,43 @@
 import { createGlobalStyle } from 'styled-components'
 
-import { colors } from './variables'
+import { colors, fonts, fontSizes, fontWeights, lineHeights } from './variables'
 
 export const GlobalStyle = createGlobalStyle`
-  color: ${colors.light};
+  color: ${colors.white};
+  background-color: ${colors.darkest};
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  p {
+    margin: 0;
+  }
+
+  h1 {
+    font-family: ${fonts.raleway};
+    font-weight: ${fontWeights.bold};
+    font-size: ${fontSizes.hero};
+    line-height: ${lineHeights.hero};
+  }
+
+  h2 {
+    font-family: ${fonts.raleway};
+    font-weight: ${fontWeights.semiBold};
+    font-size: ${fontSizes.extraLarge};
+    line-height: ${lineHeights.extraLarge};
+  }
+
+  h3 {
+
+  }
+
+  h4 {
+
+  }
+
+  h5 {
+
+  }
 `
