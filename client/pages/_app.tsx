b306dfd7e171bd8571f7db1e075f57253aaafa88
Theodor-Bjornslatt <77094997+Theodor-Bjornslatt@users.noreply.github.com>
2021-12-26 18:04:24 +0100

    Added footer and maxwidthcontainer

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from 'next/app'
 
 import { GlobalStyle } from '../styles/globalStyle'
 import { GlobalContextProvider } from '../state/GlobalState'
+import Footer from '../components/Footer'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <GlobalContextProvider>
       <GlobalStyle />
       <Component {...pageProps} />
+      <Footer />
     </GlobalContextProvider>
   )
 }
