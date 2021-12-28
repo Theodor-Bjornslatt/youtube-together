6df4c2a7b5c5453554ea750b130adfdaf85d3fc2
Theodor-Bjornslatt <77094997+Theodor-Bjornslatt@users.noreply.github.com>
2021-12-28 21:31:00 +0100

    Update MaxWidthContainer to go with new architectural standards

--- client/components/MaxWidthContainer/index.tsx
+++ client/components/MaxWidthContainer/index.tsx
@@ -1,42 +1,17 @@
-import styled from 'styled-components'
+import {
+  MaxWidthContainerStyled,
+  StyledMaxWidthProps
+} from './MaxWidthContainer.styled'
 
-import { maxWidths, sizes, spacings } from '../../styles/variables'
+type MaxWidthContainerProps = {
+  children: JSX.Element | JSX.Element[]
+} & StyledMaxWidthProps
 
-type Size = 'small' | 'medium' | 'large' | undefined
-
-type StyledMaxWidthProps = {
-  size?: Size
+export default function MaxWidthContainer({
+  children,
+  size
+}: MaxWidthContainerProps) {
+  return (
+    <MaxWidthContainerStyled size={size}>{children}</MaxWidthContainerStyled>
+  )
 }
-
-function getMaxWidth(size: Size) {
-  switch (size) {
-    case 'small':
-      return maxWidths.small
-    case 'medium':
-      return maxWidths.medium
-    case 'large':
-      return maxWidths.large
-    default:
-      return maxWidths.medium
-  }
-}
-
-export const MaxWidthContainer = styled.div<StyledMaxWidthProps>`
-  display: flex;
-  max-width: ${(props) => getMaxWidth(props?.size)}px;
-  height: 100%;
-  width: calc(100% - ${spacings.extraLarge} - ${spacings.extraLarge});
-  margin: 0 ${spacings.extraLarge};
-
-  @media screen and (max-width: ${sizes.tablet}px) {
-    width: calc(100% - ${spacings.small} - ${spacings.small});
-    margin: 0 ${spacings.small};
-  }
-
-  @media screen and (max-width: ${sizes.mobileSmall}px) {
-    width: calc(100% - ${spacings.extraSmall} - ${spacings.extraSmall});
-    margin: 0 ${spacings.extraSmall};
-  }
-`
-
-export default MaxWidthContainer
