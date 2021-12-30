0e64907d6ff979a0a304b0f954152f766c95a424
Alex <77007088+AlexEkdahl@users.noreply.github.com>
2021-12-30 17:26:14 +0100

    moved io to own scope

--- server/src/socket/socket.ts
+++ server/src/socket/socket.ts
@@ -1,30 +1,13 @@
 import { Server } from 'http'
-import socketIo from 'socket.io'
 
 import { SOCKET_EVENT as EVENT } from '../config'
-// eslint-disable-next-line import/no-cycle
+import { getIo } from './io'
 import onConnect from './controller'
 import { verify } from './middleware'
 
-let socketIO: socketIo.Server
-
-export const getIo = (): socketIo.Server => {
-  if (!socketIO) {
-    socketIO = new socketIo.Server({
-      allowEIO3: true,
-      cors: {
-        origin: true,
-        credentials: true
-      }
-    })
-  }
-  return socketIO
-}
 export const initSocket = (server: Server): void => {
   const io = getIo()
   io.attach(server)
-
   io.use(verify)
-
   io.on(EVENT.INIT, onConnect)
 }
