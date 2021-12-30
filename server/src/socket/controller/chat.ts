0b6ac75c325743dd0887a9962dee0414cc5a9162
Alex <77007088+AlexEkdahl@users.noreply.github.com>
2021-12-30 13:40:52 +0100

    now able to send to all clients in a room

--- server/src/socket/controller/chat.ts
+++ server/src/socket/controller/chat.ts
@@ -1,15 +1,21 @@
 import { ISocket } from '../../interfaces'
+// eslint-disable-next-line import/no-cycle
+import { getIo } from '../socket'
+import { idGenerator } from '../../util'
 
 interface SocketData {
   room: string
   msg: string
 }
 export function onChatMessage(this: ISocket, data: SocketData): void {
-  const obj = {
+  const message = {
     user: this.name,
     msg: data.msg,
-    timestamp: Date.now()
+    timestamp: Date.now(),
+    id: idGenerator(),
+    color: '#93FFA4'
   }
 
-  this.emit('chat', obj)
+  const io = getIo()
+  io.to(data.room).emit('chat', message)
 }
