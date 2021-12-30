1ec8d68695d6cec2a95825ff4e7b43ce5eaacf43
Alex <77007088+AlexEkdahl@users.noreply.github.com>
2021-12-30 17:27:07 +0100

    entering a room gives you all msg and clients online

--- server/src/socket/controller/room.ts
+++ server/src/socket/controller/room.ts
@@ -1,8 +1,28 @@
 import { ISocket } from '../../interfaces'
+import { Message } from '../../api/models'
 import log from '../../logger'
+import { getIo } from '../io'
 
-export function onJoinRoom(this: ISocket, room: string): void {
+interface IClient {
+  username: string | string[] | undefined
+  color: string | string[] | undefined
+}
+
+export async function onJoinRoom(this: ISocket, room: string): Promise<void> {
+  const io = getIo()
+
+  const messages = await Message.find({ room })
+  const clients = io.sockets.adapter.rooms.get(room)
+  const users: Array<IClient> = []
+
+  clients?.forEach((id: string) => {
+    const clientSocket = <ISocket>io.sockets.sockets.get(id)
+    users.push({ username: clientSocket.username, color: clientSocket.color })
+  })
+
   this.join(room)
+  io.to(this.id).emit('state', { users, messages })
+
   log.info(`${this.username} joined ${room}`)
 }
 
