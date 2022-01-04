39c938a16b24b3eaa166fc1505c009ead1f281f7
Alex <77007088+AlexEkdahl@users.noreply.github.com>
2022-01-04 09:41:01 +0100

    minor temp fix before usage of auths in next.js

--- client/pages/room/[...slug].tsx
+++ client/pages/room/[...slug].tsx
@@ -15,16 +15,39 @@ export type IMessages = {
 
 const Room = () => {
   const router = useRouter()
-  const { state } = useContext(GlobalContext)
+  const { state, dispatch } = useContext(GlobalContext)
   const { user } = state
 
   const { socket } = useSockets()
   const room = (router.query['slug'] && router.query['slug'][0]) || undefined
 
+  //THIS IS NOT THE WAY - MANDALORIAN
+  async function whoAmI() {
+    if (!user) {
+      const res = await fetch('http://localhost:8080/api/whoami', {
+        credentials: 'include'
+      })
+      const data = await res.json()
+
+      dispatch({ type: 'user', payload: data.user })
+      socket.emit('join', {
+        room,
+        username: data.user.username,
+        color: data.user.color
+      })
+    } else {
+      socket.emit('join', {
+        room,
+        username: user?.username,
+        color: user?.color
+      })
+    }
+  }
+
   useEffect(() => {
-    if (!room || !user) return
-    socket.emit('join', { room, username: user?.username, color: user?.color })
-  }, [room, user])
+    if (!room) return
+    whoAmI()
+  }, [room])
 
   return <Chat room={room}></Chat>
 }
