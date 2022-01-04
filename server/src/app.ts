4da807e5590fdc173dc9ae989b9d258e191da783
Alex <77007088+AlexEkdahl@users.noreply.github.com>
2022-01-04 11:44:30 +0100

    route done, get all rooms

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,7 +4,7 @@ import session, { Store } from 'express-session'
 import morgan from 'morgan'
 
 import { SESSION_OPTIONS, CORS_OPTIONS } from './config'
-import { authRoutes } from './api/routes'
+import { authRoutes, roomRoutes } from './api/routes'
 import { handleError } from './middleware'
 
 export const createApp = (store: Store): Express => {
@@ -21,6 +21,7 @@ export const createApp = (store: Store): Express => {
 
   // routes
   app.use('/api', authRoutes)
+  app.use('/api', roomRoutes)
 
   // 404 - no route in use
   app.use((req: Request, res: Response) => {
