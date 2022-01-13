409b14487efe20e0f655239fa3da3b348fab91b6
Alex <77007088+AlexEkdahl@users.noreply.github.com>
2022-01-13 14:48:04 +0100

    cleaned up code in getMessages

--- server/src/api/services/room.services.ts
+++ server/src/api/services/room.services.ts
@@ -13,6 +13,12 @@ interface IQueryProp {
   random?: string
 }
 
+interface IParsedQuery {
+  name?: string
+  defaultLimit: number
+  defaultPage: number
+}
+
 interface IMessageObject {
   messages: IMessage[]
   limit?: number
@@ -26,15 +32,57 @@ interface IMessageObject {
 const DEFAULT_COVER =
   'https://cdn.vox-cdn.com/thumbor/LXvoCd3sbTvxMUpVAd-f4ArjYRA=/0x0:1920x800/1820x1024/filters:focal(694x265:1000x571):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/69582511/lotr1_movie_screencaps.com_12025.0.jpg'
 
+const parseQuery = ({ limit, page, random }: IQueryProp) => {
+  const parsedLimit = limit && parseInt(limit, 10)
+  const parsedPage = page && parseInt(page, 10)
+  const parsedRandom = random && parseInt(random, 10)
+  const defaultLimit = parsedLimit || 10
+  const defaultPage = parsedPage || 1
+
+  return { defaultLimit, defaultPage, parsedRandom }
+}
+
+const getRandomMessages = async (
+  defaultRandom: number
+): Promise<IMessageObject> => {
+  const messages = await Message.aggregate([
+    { $sample: { size: defaultRandom } }
+  ])
+
+  return {
+    messages: messages.map(({ __v, _id, ...message }) => {
+      return message
+    }),
+    query: { random: defaultRandom }
+  }
+}
+
+const getQueriedMessages = async ({
+  name,
+  defaultLimit,
+  defaultPage
+}: IParsedQuery): Promise<IMessageObject> => {
+  if (!name)
+    throw new BadRequest('Specify name of the room where you want the messages')
+
+  const messages = await Message.find({ room: name })
+    .limit(defaultLimit)
+    .skip((defaultPage - 1) * defaultLimit)
+    .select('-_id -room -__v')
+
+  return {
+    messages,
+    room: name,
+    limit: defaultLimit,
+    page: defaultPage
+  }
+}
+
 export const getAllRooms = async ({
   limit,
   page
 }: IQueryProp): Promise<IRoomObject> => {
-  const parsedLimit = limit && parseInt(limit, 10)
-  const parsedPage = page && parseInt(page, 10)
-
-  const defaultLimit = parsedLimit || 10
-  const defaultPage = parsedPage || 1
+  const { defaultLimit, defaultPage } = parseQuery({ limit, page })
 
   const io = getIo()
   const rooms = await Room.find()
@@ -62,7 +110,6 @@ export const getAllRooms = async ({
 
 export const getRoomByName = async (name: string): Promise<IRoom> => {
   if (!name) throw new BadRequest('Missing params')
-
   const room = await Room.findOne({ name }).select('name -_id')
 
   return room || { size: 0 }
@@ -88,38 +135,18 @@ export const getMessages = async ({
   page,
   random
 }: IQueryProp): Promise<IMessageObject> => {
-  const parsedRandom = random && parseInt(random, 10)
-  if (parsedRandom && !name) {
-    const defaultRandom = parsedRandom > 10 ? 10 : parsedRandom
-    const messages = await Message.aggregate([
-      { $sample: { size: defaultRandom } }
-    ])
+  const { defaultLimit, defaultPage, parsedRandom } = parseQuery({
+    limit,
+    page,
+    random
+  })
 
-    return {
-      messages,
-      query: { random: parsedRandom }
-    }
-  }
+  const response =
+    parsedRandom && !name
+      ? await getRandomMessages(parsedRandom)
+      : await getQueriedMessages({ name, defaultLimit, defaultPage })
 
-  const parsedLimit = limit && parseInt(limit, 10)
-  const parsedPage = page && parseInt(page, 10)
-  const defaultLimit = parsedLimit || 10
-  const defaultPage = parsedPage || 1
-
-  if (!name)
-    throw new BadRequest('Specify name of the room where you want the messages')
-
-  const messages = await Message.find({ room: name })
-    .limit(defaultLimit)
-    .skip((defaultPage - 1) * defaultLimit)
-    .select('-_id -room -__v')
-
-  return {
-    messages,
-    room: name,
-    limit: defaultLimit,
-    page: defaultPage
-  }
+  return response
 }
 
 export const addToPlaylist = async ({
@@ -138,7 +165,7 @@ export const addToPlaylist = async ({
     }
   )
 
-  // TODO - to generic
+  // TODO - too generic
   if (updatedRoom.modifiedCount === 0) throw new Error()
 }
 
