0052a24a717b1f26a305f475a2924da683d1dbae
Alex <77007088+AlexEkdahl@users.noreply.github.com>
2022-01-13 12:04:50 +0100

    validate single item

--- server/src/validation/playlist.ts
+++ server/src/validation/playlist.ts
@@ -18,6 +18,21 @@ const validatePlaylist = (urlObject: IPlayList[]): boolean => {
   return validationResults.includes(false)
 }
 
+export const validatePlaylistItem = async (
+  item: IPlayList | undefined
+): Promise<void> => {
+  const validRegex =
+    /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w-]+\?v=|embed\/|v\/)?)([\w-]+)(\S+)?$/
+
+  if (!item?.url) {
+    throw new BadRequest('Must provide a url')
+  }
+
+  if (!validRegex.test(item.url)) {
+    throw new BadRequest('Youtube url is not valid')
+  }
+}
+
 export const validateRoom = async ({
   name,
   playlist
