121be8fc38623417420b4b9635eb39fdcceba9dc
Alex <77007088+AlexEkdahl@users.noreply.github.com>
2022-01-18 18:32:33 +0100

    responsive video

--- client/components/Video/Video.styled.tsx
+++ client/components/Video/Video.styled.tsx
@@ -3,8 +3,26 @@ import styled from 'styled-components'
 
 import { colors } from '../../styles/variables'
 
+export const ContentContainer = styled.div`
+  max-width: 1000px;
+`
+
+export const VideoBoundary = styled.div`
+  max-height: 562px;
+`
+
+export const VideoContainer = styled.div`
+  position: relative;
+  padding-bottom: 50.25%;
+  height: 0;
+`
+
 export const VideoPlayer = styled(ReactPlayer)`
   pointer-events: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  max-width: 1000px;
 `
 
 export const PauseOverlay = styled.div`
